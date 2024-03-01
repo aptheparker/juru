0949da6f3f4612fa7e4c5ea058690a32dee17bc3
aptheparker <apapap1119@g.skku.edu>
2024-03-02 00:38:02 +0900

    feat: first init with routes

---
+++ app/board-page/page.tsx
@@ -0,0 +1,7 @@
+export default function BoardPage() {
+  return (
+    <>
+      <h1>Board Page</h1>
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Button from "./components/button";
+
+export default function HomePage() {
+  return (
+    <section className="flex justify-center items-center h-screen">
+      <div className="text-center">
+        <h1>Home</h1>
+        <Button />
+      </div>
+    </section>
+  );
+}
