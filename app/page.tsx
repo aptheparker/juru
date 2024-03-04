fdb5c495f7694327d002e4317a33ee9c24fbdbc9
aptheparker <apapap1119@g.skku.edu>
2024-03-05 00:42:51 +0900

    fix: layout for body added

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@ import Button from "./components/button";
 
 export default function HomePage() {
   return (
-    <section className="flex justify-center items-center h-screen">
-      <div className="text-center">
-        <h1>Home</h1>
-        <Button />
-      </div>
-    </section>
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-4">Juru</h1>
+      <Button />
+    </div>
   );
 }
