a224418e30b1a09422df57d7cfad49d2a13882e1
aptheparker <apapap1119@g.skku.edu>
2024-03-05 13:18:58 +0900

    feat: background color updated & blur not complete

--- app/board-page/page.tsx
+++ app/board-page/page.tsx
@@ -22,11 +22,14 @@ export default function BoardPage() {
   function handleConfirm() {
     setIsFirstRender(false);
     setConfirmedNumOfPlayers(numOfPlayers);
-    console.log(numOfPlayers);
   }
 
   return (
-    <div className="text-center w-full h-full flex flex-col items-center justify-center">
+    <div
+      className={`text-center w-full h-full flex flex-col items-center justify-center ${
+        isFirstRender ? "backdrop-blur-sm bg-white/30" : "bg-green-300"
+      }`}
+    >
       {isFirstRender && (
         <PlayerCard
           numOfPlayers={numOfPlayers}
@@ -35,8 +38,9 @@ export default function BoardPage() {
           confirmNumOfPlayers={handleConfirm}
         />
       )}
-      <h1 className="text-4xl font-bold">Board Page</h1>
-      <p className="text-xl">Number of players: {confirmedNumOfPlayers}</p>
+
+      <h1 className="text-4xl font-bold">Juru Board Game</h1>
+      <p className="text-xl">Number of Players: {confirmedNumOfPlayers}</p>
       <JuruBoard numOfPlayers={confirmedNumOfPlayers} />
     </div>
   );
--- components/card/player-card.tsx
+++ components/card/player-card.tsx
@@ -10,8 +10,12 @@ export default function PlayerCard(prop: {
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded shadow-lg space-y-4 w-96">
       <h1>Number of players: {prop.numOfPlayers}</h1>
       <div className="flex justify-between">
-        <Button onClick={prop.addNumOfPlayers} width="45%" height="40px">Add player</Button>
-        <Button onClick={prop.removeNumOfPlayers} width="45%" height="40px">Remove player</Button>
+        <Button onClick={prop.addNumOfPlayers} width="45%" height="40px">
+          Add player
+        </Button>
+        <Button onClick={prop.removeNumOfPlayers} width="45%" height="40px">
+          Remove player
+        </Button>
       </div>
       <Button onClick={prop.confirmNumOfPlayers}>Confirm</Button>
     </div>
