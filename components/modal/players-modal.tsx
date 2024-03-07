f5ccc1055236960d3df80b1c8a0f107ba291d3a2
Ji__ho <cale.jiho@gmail.com>
2024-03-07 09:35:21 +0900

    feat: Confirm BackDrop Created

--- components/modal/players-modal.tsx
+++ components/modal/players-modal.tsx
@@ -7,17 +7,20 @@ export default function PlayersModal(prop: {
   confirmNumOfPlayers: () => void;
 }) {
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded shadow-lg space-y-4 w-96 border border-black">
-      <h1>Number of players: {prop.numOfPlayers}</h1>
-      <div className="flex justify-between">
-        <Button onClick={prop.addNumOfPlayers} width="45%" height="40px">
-          Add player
-        </Button>
-        <Button onClick={prop.removeNumOfPlayers} width="45%" height="40px">
-          Remove player
-        </Button>
+    <>
+      <div className="absolute w-full h-full bg-gray-400/50" />
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded shadow-lg space-y-4 w-96 border border-black">
+        <h1>Number of players: {prop.numOfPlayers}</h1>
+        <div className="flex justify-between">
+          <Button onClick={prop.addNumOfPlayers} width="45%" height="40px">
+            Add player
+          </Button>
+          <Button onClick={prop.removeNumOfPlayers} width="45%" height="40px">
+            Remove player
+          </Button>
+        </div>
+        <Button onClick={prop.confirmNumOfPlayers}>Confirm</Button>
       </div>
-      <Button onClick={prop.confirmNumOfPlayers}>Confirm</Button>
-    </div>
+    </>
   );
 }
