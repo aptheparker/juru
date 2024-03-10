7ffaae3c5acc39b98e6887cc5817f76e2b7348d5
aptheparker <apapap1119@g.skku.edu>
2024-03-10 16:43:33 +0900

    fix: suspense tag added for useSearchParams

--- app/board-page/page.tsx
+++ app/board-page/page.tsx
@@ -1,11 +1,10 @@
-'use client'
+"use client";
 
 import PlayersModal from "@/components/modal/players-modal";
 import JuruBoard from "@/components/board/juru-board";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 export default function BoardPage() {
-
   const [isFirstRender, setIsFirstRender] = useState(true);
   const [numOfPlayers, setNumOfPlayers] = useState(1);
   const [confirmedNumOfPlayers, setConfirmedNumOfPlayers] = useState(1);
@@ -26,23 +25,25 @@ export default function BoardPage() {
   }
 
   return (
-    <div
-      className={`text-center w-full h-full flex flex-col items-center justify-center ${
-        isFirstRender ? "backdrop-blur-sm bg-white/30" : "bg-green-300"
-      }`}
-    >
-      {isFirstRender && (
-        <PlayersModal
-          numOfPlayers={numOfPlayers}
-          addNumOfPlayers={addNumOfPlayers}
-          removeNumOfPlayers={removeNumOfPlayers}
-          confirmNumOfPlayers={handleConfirm}
-        />
-      )}
+    <Suspense>
+      <div
+        className={`text-center w-full h-full flex flex-col items-center justify-center ${
+          isFirstRender ? "backdrop-blur-sm bg-white/30" : "bg-green-300"
+        }`}
+      >
+        {isFirstRender && (
+          <PlayersModal
+            numOfPlayers={numOfPlayers}
+            addNumOfPlayers={addNumOfPlayers}
+            removeNumOfPlayers={removeNumOfPlayers}
+            confirmNumOfPlayers={handleConfirm}
+          />
+        )}
 
-      <h1 className="text-4xl font-bold">Juru Board Game</h1>
-      <p className="text-xl">Number of Players: {confirmedNumOfPlayers}</p>
-      <JuruBoard numOfPlayers={confirmedNumOfPlayers} />
-    </div>
+        <h1 className="text-4xl font-bold">Juru Board Game</h1>
+        <p className="text-xl">Number of Players: {confirmedNumOfPlayers}</p>
+        <JuruBoard numOfPlayers={confirmedNumOfPlayers} />
+      </div>
+    </Suspense>
   );
 }
